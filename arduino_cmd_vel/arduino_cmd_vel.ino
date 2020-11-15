444c9edb33a8d06e09fc5566f9b620f4d3da1b79
yq <yq@lhcb.localdomain>
2020-11-15 18:25:15 -0500

    arduino code

---
+++ arduino_cmd_vel/arduino_cmd_vel.ino
@@ -0,0 +1,102 @@
+/**
+ * Subscribe to a cmd_vel topic and move the robot.
+ * Yongming Qin
+ * 2020_1115
+ */
+
+#include <math.h>
+#include <ros.h>
+#include <geometry_msgs/Twist.h>
+
+// The min amount of PWM the motors need to move. Depends on the battery, motors and controller.
+// The max amount is defined by PWMRANGE in Arduino.h
+#define PWM_MIN 40
+#define PWMRANGE 80
+
+
+// Declare functions
+void setupPins();
+void stop();
+void callback_twist(const geometry_msgs::Twist &msg);
+float mapPwm(float x, float out_min, float out_max);
+
+// Pins
+const uint8_t L_FORW = 4;
+const uint8_t L_BACK = 2;
+const uint8_t L_PWM = 3;
+const uint8_t R_FORW = 7;
+const uint8_t R_BACK = 5;
+const uint8_t R_PWM = 6;
+
+
+ros::NodeHandle nh;
+ros::Subscriber<geometry_msgs::Twist> sub("/cmd_vel", &callback_twist );
+
+void setup()
+{
+  setupPins();
+  nh.initNode();
+  nh.subscribe(sub);
+}
+
+void loop()
+{
+  if (!nh.connected()) {
+    stop();
+  }
+  nh.spinOnce();
+}
+
+void callback_twist( const geometry_msgs::Twist &msg) {
+  if (!nh.connected()) {
+    stop();
+    return;
+  }
+  // Cap values at [-1 .. 1]
+  float x = max(min(msg.linear.x, 1.0f), -1.0f);
+  float z = max(min(msg.angular.z, 1.0f), -1.0f);
+
+  // Calculate the intensity of left and right wheels. Simple version.
+  // Taken from https://hackernoon.com/unicycle-to-differential-drive-courseras-control-of-mobile-robots-with-ros-and-rosbots-part-2-6d27d15f2010#1e59
+  float l = (msg.linear.x - msg.angular.z) / 2;
+  float r = (msg.linear.x + msg.angular.z) / 2;
+
+  // Then map those values to PWM intensities. PWMRANGE = full speed, while PWM_MIN = the minimal amount of power at which the motors begin moving.
+  uint16_t lPwm = mapPwm(fabs(l), PWM_MIN, PWMRANGE);
+  uint16_t rPwm = mapPwm(fabs(r), PWM_MIN, PWMRANGE);
+
+  // Set direction pins and PWM
+  digitalWrite(L_FORW, l > 0);
+  digitalWrite(L_BACK, l < 0);
+  digitalWrite(R_FORW, r > 0);
+  digitalWrite(R_BACK, r < 0);
+  analogWrite(L_PWM, lPwm);
+  analogWrite(R_PWM, rPwm);
+}
+
+void setupPins()
+{
+  pinMode(L_PWM, OUTPUT);
+  pinMode(L_FORW, OUTPUT);
+  pinMode(L_BACK, OUTPUT);
+  pinMode(R_PWM, OUTPUT);
+  pinMode(R_FORW, OUTPUT);
+  pinMode(R_BACK, OUTPUT);
+  stop();
+}
+
+void stop()
+{
+  digitalWrite(L_FORW, 0);
+  digitalWrite(L_BACK, 0);
+  digitalWrite(R_FORW, 0);
+  digitalWrite(R_BACK, 0);
+  analogWrite(L_PWM, 0);
+  analogWrite(R_PWM, 0);
+}
+
+// Map x value from [0 .. 1] to [out_min .. out_max]
+float mapPwm(float x, float out_min, float out_max)
+{
+  return x * (out_max - out_min) + out_min;
+}
